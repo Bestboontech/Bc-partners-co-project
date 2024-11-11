e805b9bc0f3c187fdbed15b950f9e8eb1ca810c2
sammykae <sammykay301@live.com>
2024-11-11 18:19:01 +0100

    log

--- api/send-email.js
+++ api/send-email.js
@@ -1,16 +1,19 @@
 export default async function handler(req, res) {
-    if (req.method !== 'POST') {
-      return res.status(405).json({ error: 'Method not allowed' });
-    }
-  
-    const { formData } = req.body;
-  
-    // Send email using EmailJS API
-    try {
-      const response = await fetch('https://api.emailjs.com/api/v1.0/email/send-form', {
-        method: 'POST',
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Method not allowed" });
+  }
+
+  const { formData } = req.body;
+  console.log(formData);
+
+  // Send email using EmailJS API
+  try {
+    const response = await fetch(
+      "https://api.emailjs.com/api/v1.0/email/send-form",
+      {
+        method: "POST",
         headers: {
-          'Content-Type': 'multipart/form-data',
+          "Content-Type": "multipart/form-data",
         },
         body: JSON.stringify({
           service_id: process.env.EMAILJS_SERVICE_ID,
@@ -18,15 +21,16 @@ export default async function handler(req, res) {
           user_id: process.env.EMAILJS_USER_ID,
           ...formData,
         }),
-      });
-  
-      if (response.ok) {
-        res.status(200).json({ message: 'Your mail is sent!' });
-      } else {
-        const error = await response.json();
-        res.status(500).json({ error });
       }
-    } catch (error) {
-      res.status(500).json({ error: error.message });
+    );
+
+    if (response.ok) {
+      res.status(200).json({ message: "Your mail is sent!" });
+    } else {
+      const error = await response.json();
+      res.status(500).json({ error });
     }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
+}
